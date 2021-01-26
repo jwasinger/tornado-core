c255099a2a2640f906629cc5455af8465ae95c96
Alex Beregszaszi <alex@rtfs.hu>
2021-01-26 10:28:31 +0000

    Add hasher build code for evm384

--- compileHasher.js
+++ compileHasher.js
@@ -3,16 +3,25 @@
 const path = require('path')
 const fs = require('fs')
 const genContract = require('circomlib/src/mimcsponge_gencontract.js')
+const assert = require('assert')
 
 // where Truffle will expect to find the results of the external compiler
 // command
 const outputPath = path.join(__dirname, 'build', 'Hasher.json')
 
+function evm384 (path) {
+  const deployed = fs.readFileSync(path).toString().replace('\n', '').replace('0x', '')
+  let length = deployed.length.toString(16)
+  assert(length.length == 4)
+  return '0x38600c60003961' + length + '6000f3' + deployed
+}
+
 function main () {
   const contract = {
     contractName: 'Hasher',
     abi: genContract.abi,
-    bytecode: genContract.createCode('mimcsponge', 220)
+    bytecode: evm384('./mimc_cipher.hex')
+    //bytecode: genContract.createCode('mimcsponge', 220)
   }
 
   fs.writeFileSync(outputPath, JSON.stringify(contract))
